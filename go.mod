6b2b0bb4429cfee0c9d80afcce39c36fa9b7973c
ashpect <ashishndiitr@gmail.com>
2025-10-23 01:32:24 +0530

    test

--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module ashpect/gnarkexp
 
 go 1.25.0
 
-require github.com/ingonyama-zk/icicle/v3 v3.9.2
-
-require github.com/stretchr/testify v1.10.0 // indirect
+require github.com/ingonyama-zk/icicle-gnark/v3 v3.2.2
--- main.go
+++ main.go
@@ -1,58 +1,20 @@
 package main
 
 import (
-	"github.com/ingonyama-zk/icicle/v3/wrappers/golang/core"
-	"github.com/ingonyama-zk/icicle/v3/wrappers/golang/curves/bn254"
-	"github.com/ingonyama-zk/icicle/v3/wrappers/golang/curves/bn254/msm"
-	"github.com/ingonyama-zk/icicle/v3/wrappers/golang/runtime"
+	"fmt"
+
+	"github.com/ingonyama-zk/icicle-gnark/v3/wrappers/golang/runtime"
+
 )
 
 func main() {
-	// Load backend using env path
-	runtime.LoadBackendFromEnvOrDefault()
-	// Set Cuda device to perform
-	device := runtime.CreateDevice("CUDA", 0)
-	runtime.SetDevice(&device)
 
-	// Obtain the default MSM configuration.
-	cfg := core.GetDefaultMSMConfig()
-
-	// Define the size of the problem, here 2^18.
-	size := 1 << 18
-
-	// Generate scalars and points for the MSM operation.
-	scalars := bn254.GenerateScalars(size)
-	points := bn254.GenerateAffinePoints(size)
-
-	// Create a CUDA stream for asynchronous operations.
-	stream, _ := runtime.CreateStream()
-	var p bn254.Projective
-
-	// Allocate memory on the device for the result of the MSM operation.
-	var out core.DeviceSlice
-	_, e := out.MallocAsync(p.Size(), 1, stream)
-
+	devices, e := runtime.GetRegisteredDevices()
 	if e != runtime.Success {
-		panic(e)
+		panic("Failed to load registered devices")
 	}
 
-	// Set the CUDA stream in the MSM configuration.
-	cfg.StreamHandle = stream
-	cfg.IsAsync = true
+	fmt.Println("len(devices): ", len(devices))
+	fmt.Println("Devices: ", devices)
 
-	// Perform the MSM operation.
-	e = msm.Msm(scalars, points, &cfg, out)
-
-	if e != runtime.Success {
-		panic(e)
-	}
-
-	// Allocate host memory for the results and copy the results from the device.
-	outHost := make(core.HostSlice[bn254.Projective], 1)
-	runtime.SynchronizeStream(stream)
-	runtime.DestroyStream(stream)
-	outHost.CopyFromDevice(&out)
-
-	// Free the device memory allocated for the results.
-	out.Free()
 }
